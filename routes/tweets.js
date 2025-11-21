c097e917868ca33893491b01cf6931eaf042e6c6
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-21 09:45:05 +0100

    fix all route

--- routes/tweets.js
+++ routes/tweets.js
@@ -54,10 +54,11 @@ router.delete("/delete/:tweetid", async (req, res) => {
 
     res.json({ result: true, message: "Tweet deleted successfully" });
 });
-
 router.get('/all', async (req, res) => {
     try {
-        const tweets = await Tweet.find().populate("author", "username", "firstname").sort({ date: -1 });
+        const tweets = await Tweet.find()
+            .populate("author", "username firstname")
+            .sort({ date: -1 });
 
         const formattedTweets = tweets.map(t => ({
             id: t._id,
@@ -68,10 +69,10 @@ router.get('/all', async (req, res) => {
             likes: t.usersWhoLiked.length,
         }));
 
-        res.json({ result: true, tweets: formattedTweets })
+        res.json({ result: true, tweets: formattedTweets });
     } catch (error) {
         res.status(500).json({ result: false, error: "Server error" });
     }
-})
+});
 
 module.exports = router;
