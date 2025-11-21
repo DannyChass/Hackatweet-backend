365e58294b336549c9d26929c14b6ceb5f0059a5
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-21 10:49:56 +0100

    add trend all route

---
+++ routes/trends.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const Trend = require("../models/Trend");
+
+router.get("/all", async (req, res) => {
+    try {
+        let trends = await Trend.find().lean();
+
+        trends.sort((a, b) => b.tweets.length - a.tweets.length);
+
+        const formatted = trends.map(t => ({
+            name: t.name,
+            tweetCount: t.tweets.length
+        }));
+
+        res.json({ result: true, trends: formatted });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ result: false, error: "Server error" });
+    }
+})
